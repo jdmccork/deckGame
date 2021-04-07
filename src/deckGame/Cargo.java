59e68c40f92978a2575a55a1368e1368be462599
gha95 <gha95@uclive.ac.nz>
2021-04-07 12:42:59 +1200

    Cargo Class
    Cargo class implemented as extension of Item, UML class diagram changed accordingly

--- src/deckGame/Cargo.java
+++ src/deckGame/Cargo.java
@@ -1,5 +1,23 @@
 package deckGame;
 
-public class Cargo {
-
+public class Cargo extends Item{
+	private String modifyStat;
+	private int modifyAmount;
+	
+	Cargo(String name, String description, int size, int basePrice, String rarity, String modifyStat, int modifyAmount) {
+		super(name, description, size, basePrice, rarity);
+		//ModifyStat can be null if the item does not change a stat.
+		this.modifyStat = modifyStat;
+		this.modifyAmount = modifyAmount;
+	}
+	
+	public String ToString() {
+		String output = super.ToString();
+		if (modifyStat != null) {
+			output += "\nThis modifies your "+modifyStat+" stat by "+modifyAmount+".";
+		} else {
+			output += "\nThis does not affect any of your stats.";
+		}
+		return output;
+	}
 }
